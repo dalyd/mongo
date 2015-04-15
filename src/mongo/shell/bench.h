11948c874a8bb87fb931bc68ff44d841a6035342
dalyd <david.daly@mongodb.com>
2015-04-15 11:06:31 -0400

    Added explicit count of operations executed in bench.cpp loop

--- src/mongo/shell/bench.h
+++ src/mongo/shell/bench.h
@@ -246,6 +246,7 @@ namespace mongo {
         BenchRunEventCounter insertCounter;
         BenchRunEventCounter deleteCounter;
         BenchRunEventCounter queryCounter;
+        BenchRunEventCounter opCounter;
 
         std::map<std::string, long long> opcounters;
         std::vector<BSONObj> trappedErrors;
